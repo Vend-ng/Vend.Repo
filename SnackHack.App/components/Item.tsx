76d4bb87e9b270a7d73566ebee2c7cfab050fb0c
Jacques Bourquin <jacquesaronius@gmail.com>
2019-10-31 05:16:27 -0500

    Added a bunch of stuff

--- SnackHack.App/components/Item.tsx
+++ SnackHack.App/components/Item.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { Component }  from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableHighlight, Alert } from 'react-native';
 import NumberFormat  from 'react-number-format';
 import PropTypes from 'prop-types';
 import { styles } from '../styles/styles';
+
+var images = [];
+
+images["3389"] = require('../assets/3389.png');
+images["6351"] = require('../assets/6351.png');
+images["1548"] = require('../assets/1548.png');
+images["4986"] = require('../assets/4986.png');
+images["2897"] = require('../assets/2897.png');
+images["2158"] = require('../assets/2158.png');
+images["9513"] = require('../assets/9513.png');
+images["8298"] = require('../assets/8298.png');
 
 export default class Item extends Component<
     { id: number, 
       description:string, 
       volume:number, 
       price:number}> {
-
     render() {
         return(
-        <View style={styles.item}>
-            <Image style={{width: 107, height: 332}} source={require('../assets/1.png')} />
-            <Text>{this.props.description + " " + this.props.volume} oz.</Text>
-            <NumberFormat value={this.props.price} 
-                displayType={'text'} 
-                thousandSeparator={true} 
-                prefix={'$'}
-                decimalScale={2}
-                fixedDecimalScale={true}
-                renderText={value => <Text style={{textAlign: "center",}}>{value}</Text>} />
-        </View>)
+        <TouchableHighlight onPress={() => setTimeout(() => (Alert.alert("Remption Code", "Please enter this code into the machine\n\n" + this.props.id)), 3000)}>
+            <View style={styles.item}>
+                <Image style={{width: 53, height: 166, alignContent: "center"}} source={images[this.props.id.toString()]} />
+                <Text style={styles.item_text}>{this.props.description + " " + this.props.volume} oz.</Text>
+                <NumberFormat value={this.props.price} 
+                    displayType={'text'} 
+                    thousandSeparator={true} 
+                    prefix={'$'}
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    renderText={value => <Text style={styles.item_text}>{value}</Text>} />
+            </View>
+        </TouchableHighlight>)
     }
 }
--- SnackHack.App/screens/ItemScreen.tsx
+++ SnackHack.App/screens/ItemScreen.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
 import { Component }  from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { styles } from '../styles/styles';
 import Item from '../components/Item'
 
 export default class ItemScreen extends Component {
     render() {
         return(
-            <View style={styles.container}>
-                <Text>Item screen</Text>
-                <Item id={1}
-                      description="Coca-Cola" 
-                      volume={20} 
-                      price={1.50} /> 
+            <View style={styles.items_container}>
+                <Text style={styles.items_text}>Test Machine</Text>
+                <ScrollView>
+                    <View style={styles.items_scroll}>
+                    <Item id={3389}
+                        description="Coca-Cola" 
+                        volume={20} 
+                        price={1.50} /> 
+                    <Item id={6351}
+                        description="Diet Coke" 
+                        volume={20} 
+                        price={1.50} /> 
+                    <Item id={1548}
+                        description="Dr. Pepper" 
+                        volume={20} 
+                        price={1.50} /> 
+                    <Item id={4986}
+                        description="Sprite" 
+                        volume={20} 
+                        price={1.50} /> 
+                    <Item id={2897}
+                        description="Gold Peak Sweet Tea" 
+                        volume={20} 
+                        price={1.75} /> 
+                    <Item id={2158}
+                        description="Gold Peak Unsweet Tea" 
+                        volume={20} 
+                        price={1.75} /> 
+                    <Item id={9513}
+                        description="Barq's" 
+                        volume={20} 
+                        price={1.50} /> 
+                    <Item id={8298}
+                        description="Fanta" 
+                        volume={20} 
+                        price={1.50} />
+                    </View>
+                </ScrollView>
             </View>
         );
     }
