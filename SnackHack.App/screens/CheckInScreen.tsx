680f45ebf13d973787fabf153c3f7a477ae7e9b3
Jacques Bourquin <jacquesaronius@gmail.com>
2019-11-21 17:00:45 -0600

    Working on navigation

---
+++ SnackHack.App/screens/CheckInScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component }  from 'react';
+import { Text, View } from 'react-native';
+import { styles } from '../styles/styles';
+
+export default class CheckInScreen extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
